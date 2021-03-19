b738a924c1da44ca7aa8f27a34d21998d89b3344
Renan Paiva Bruce <bruce.renan@hotmail.com>
2021-03-18 23:30:53 -0400

    laravel sanctum

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
+
+class UserController extends Controller
+{
+
+    /**
+     * Login
+     * @param Request $request     
+     */
+    public function login(Request $request){
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',            
+        ]);
+    
+        $user = User::where('email', $request->email)->first();
+    
+        if (! $user || ! Hash::check($request->password, $user->password)) {
+            throw ValidationException::withMessages([
+                'email' => ['The provided credentials are incorrect.'],
+            ]);
+        }
+    
+        return $user->createToken($request->email)->plainTextToken;
+    }
+
+    /**
+     * Request
+     * @param Request $request     
+     */
+    public function index()
+    {
+        return User::all();
+    }
+}
---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Support\Str;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::create([
+            'name' => 'Teste T.',
+            'email' => 'teste@teste.com',
+            'email_verified_at' => now(),
+            'password' => bcrypt('teste'), // teste
+            'remember_token' => Str::random(10),
+        ]);
+
+        // 'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+    }
+}
